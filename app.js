d11e454b48f94aac4ac95fd6f6b262a44fc31a2b
bws.ksi <bws.ksi@gmail.com>
2024-02-06 22:53:19 +0200

    creating simple server

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-const { error } = require('console')
 const http = require('http')
+
+const PORT = 3000
+
 const server = http.createServer((req, res) => {
   console.log('Server request')
-  console.log(req.url, req.method)
 
-  res.setHeader('Content-Type', 'application/json')
-  // res.write('<head><link rel="stylesheet" href="#"></head>')
-  // res.write('<h1>Hello node</h1>')
-  // res.write('<p>My name lorem</p>')
-
-  const data = JSON.stringify([
-    { name: 'Tommy', age: 35 },
-    { name: 'Arthur', age: 40 }
-  ])
-
-  res.end(data)
+  res.setHeader('Content-Type', 'text/html')
 })
 
-server.listen(3000, 'localhost', error => {
-  error ? console.log(error) : console.log('listening port 3000')
+server.listen(PORT, 'localhost', error => {
+  error ? console.log(error) : console.log(`listening port ${PORT}`)
 })
