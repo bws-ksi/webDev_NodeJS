ae329826e195afca41e226599c8b5fa32dcf29d6
bws.ksi <bws.ksi@gmail.com>
2024-02-06 23:13:10 +0200

    add simple routing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const PORT = 3000
 
@@ -6,6 +8,31 @@ const server = http.createServer((req, res) => {
   console.log('Server request')
 
   res.setHeader('Content-Type', 'text/html')
+
+  const createPath = page => path.resolve(__dirname, 'views', `${page}.html`)
+
+  let basePath = ''
+
+  switch (req.url) {
+    case '/':
+      basePath = createPath('index')
+      break
+    case '/contacts':
+      basePath = createPath('contacts')
+      break
+    default:
+      basePath = createPath('error')
+      break
+  }
+  fs.readFile(basePath, (err, data) => {
+    if (err) {
+      console.log(err)
+      res.end()
+    } else {
+      res.write(data)
+      res.end()
+    }
+  })
 })
 
 server.listen(PORT, 'localhost', error => {
